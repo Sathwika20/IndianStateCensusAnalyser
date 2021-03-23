fdff8aac6d868ad912b2532c81855ba22ce14512
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-03-23 18:40:45 +0530

    Added code to State Census CSV File if incorrect Returns a custom Exception

--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 
 public class CensusAnalyserTest {
     public static final String INDIAN_STATE_CENSUS_FILE = "C:\\Users\\gopir\\IdeaProjects\\IndianStateCensusAnalyserProblem\\src\\main\\resources\\IndiaStateCensusData.csv";
-    
+    private static final String WRONG_CSV_FILE = "C:\\Users\\gopir\\IdeaProjects\\IndianStateCensusAnalyserProblem\\src\\main\\resources\\IndiaStateCode";;
+
     @Test
     public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() {
         try {
@@ -15,6 +16,14 @@ public class CensusAnalyserTest {
             Assert.assertEquals(29, count);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenIndiaStateCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser.loadCensusData(WRONG_CSV_FILE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
     }
 
